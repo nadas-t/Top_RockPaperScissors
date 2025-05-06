d0d00c36aae0a65e2b68a1647a8441a3fa3412b1
Thiago Nadim <nadimthiago@gmail.com>
2025-05-06 13:03:53 -0300

    rock paper scissors game

---
+++ script.js
@@ -0,0 +1,87 @@
+
+const getComputerChoice=()=>{
+    let choice= Math.floor(9*Math.random())
+    console.log(choice)
+    if (choice<=2){
+        return("rock");
+    }
+    else if ( choice <=5){
+        return("paper");
+    }
+    else{
+        return("scissors");
+    }
+}
+
+// console.log(getComputerChoice())
+
+const getHumanChoice=()=>{
+    let answer=prompt("qual sua escolha,humano?!!");
+    return answer
+}
+// console.log(getHumanChoice())
+
+
+
+const playRound=(humanChoice,computerChoice)=>{
+    humanChoice=humanChoice.toLowerCase();
+    computerChoice=computerChoice.toLowerCase();
+
+    if (humanChoice=="paper"){
+        if(computerChoice=="paper"){
+            console.log(`It's a draw!Both got ${humanChoice}`)
+        }
+        else if(computerChoice=="rock"){
+            console.log(`You win!${humanChoice} beats ${computerChoice}`)
+            humanScore+=1;
+        }
+        else{
+            console.log(`You lose!${computerChoice} beats ${humanChoice}!`)
+            computerScore+=1
+        }
+    }
+    else if (humanChoice=="rock"){
+        if(computerChoice=="paper"){
+            console.log(`You lose!${computerChoice} beats ${humanChoice}!`)
+            computerScore+=1;
+        }
+        else if(computerChoice=="rock"){
+            console.log(`It's a draw!Both got ${humanChoice}`)
+        }
+        else{
+            console.log(`You win!${humanChoice} beats ${computerChoice}`)
+            humanScore+=1;
+        }
+    }
+    else{
+        if(computerChoice=="paper"){
+            console.log(`You win!${humanChoice} beats ${computerChoice}`)
+            humanScore+=1;
+        }
+        else if(computerChoice=="rock"){
+            console.log(`You lose!${computerChoice} beats ${humanChoice}!`)
+            computerScore+=1;
+        }
+        else{
+            console.log(`It's a draw!Both got ${humanChoice}`)
+        }
+    }
+
+}
+
+const playGame=()=>{
+    for(let i=0;i<5;i++){
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+    if (humanScore>computerScore){
+        console.log(`human is the winner with ${humanScore} points`)
+    }
+    else{
+        console.log(`computer is the winner with ${computerScore} points`)
+
+    }
+}
+
+let humanScore=0;
+let computerScore=0;
+playGame()
